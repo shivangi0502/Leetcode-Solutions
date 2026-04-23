528109d16d29375b1a38cacf7879a88e3f852f50
shivangi0502 <168461312+shivangi0502@users.noreply.github.com>
2026-04-23 10:11:16 +0000

    Sync LeetCode submission Runtime - 39 ms (94.97%), Memory - 29.2 MB (46.55%)

--- 0015-3sum/solution.cpp
+++ 0015-3sum/solution.cpp
@@ -2,27 +2,38 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        for(int i = 0; i<n; i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int j = i+1, k=n-1;
+        vector<vector<int>> ans;
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            
+            int j = i+1;
+            int k = n-1;
+            int s = -1*nums[i];
             while(j<k){
-                int sum = nums[i]+ nums[j] + nums[k];
-                if(sum<0){
-                    j++;
+                int sum = nums[j]+nums[k];
+                if(sum==s){
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;k--;
+                    while(j<k && nums[j]==nums[j-1]){
+                        j++;
+                    }
+                    while(j<k && nums[k]==nums[k+1]){
+                         k--;
+                    }
                 }
-                else if(sum>0){
+                else if(sum>s){
                     k--;
                 }
                 else{
-                    ans.push_back({nums[i], nums[j], nums[k]});
-                    j++;k--;
-                    while(j<k && nums[j] == nums[j-1]) j++;
-
+                    j++;
                 }
             }
         }
+        
         return ans;
+        
     }
 };
